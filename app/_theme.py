b8ed8d20ce385730ae11419aa15c01c0bfd17dc2
orvad <martin@metalab.dk>
2024-02-14 13:20:04 +0100

    Update EXAMPLE.env and _theme.py

--- app/_theme.py
+++ app/_theme.py
@@ -8,10 +8,8 @@ from nicegui import ui
 @contextmanager
 def frame():
     with ui.header().classes("justify-between text-white items-center"):
-        ui.button(icon="home", on_click=lambda: ui.open("/")).props(
-            "color=positive round size=sm"
-        )
-        ui.label("Modularization Example").classes("font-bold text-lg")
+        ui.button(icon="home", on_click=lambda: ui.open("/")).props("round size=sm")
+        ui.label(os.environ.get("APP_NAME", "My App")).classes("font-bold text-lg")
 
     with ui.element("div").classes("text-center w-full md:w-1/2 mx-auto"):
         yield
