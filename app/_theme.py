80b0f8474f2df0f769e03f24257b53c85a6d95c1
orvad <martin@metalab.dk>
2024-02-14 13:16:53 +0100

    Refactor header styling and add label in _theme.py

--- app/_theme.py
+++ app/_theme.py
@@ -7,10 +7,11 @@ from nicegui import ui
 
 @contextmanager
 def frame():
-    with ui.element("div").classes("text-center w-full"):
-        url = "/"
-        ui.button(icon="home", on_click=lambda: ui.open(url)).props(
-            "color=secondary round"
+    with ui.header().classes("justify-between text-white items-center"):
+        ui.button(icon="home", on_click=lambda: ui.open("/")).props(
+            "color=positive round size=sm"
         )
+        ui.label("Modularization Example").classes("font-bold text-lg")
+
     with ui.element("div").classes("text-center w-full md:w-1/2 mx-auto"):
         yield
