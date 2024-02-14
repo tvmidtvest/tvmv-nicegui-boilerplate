f67093c1f50a1d37a88d2a4494e498a91a59c525
orvad <martin@metalab.dk>
2024-02-14 12:39:00 +0100

    Add MY_VAR environment variable and log its value in page content

--- app/page.py
+++ app/page.py
@@ -9,3 +9,7 @@ logger = config.get_logger(__name__)
 def content():
     ui.markdown("Welcome to the other page!")
     ui.button("Go to the home page", on_click=lambda: ui.open("/"))
+
+    my_var = os.environ.get("MY_VAR", "default value")
+
+    logger.info(f"Page content was displayed: {my_var}")
