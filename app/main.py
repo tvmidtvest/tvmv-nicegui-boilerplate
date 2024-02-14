4f355b8daa5628ec8745fbc9baca22bc89cfce29
orvad <martin@metalab.dk>
2024-02-14 13:05:06 +0100

    Remove deprecated theme module and import _theme instead

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 import os
 
+import _theme
 import config
 import home
 import page
-import theme
 from nicegui import ui
 
 # Put this in all files that use the logger
@@ -12,13 +12,13 @@ logger = config.get_logger(__name__)
 
 @ui.page("/")
 def index_page() -> None:
-    with theme.frame():
+    with _theme.frame():
         home.content()
 
 
 @ui.page("/page")
 def index_page() -> None:
-    with theme.frame():
+    with _theme.frame():
         page.content()
 
 
